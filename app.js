a483bda4753f34613876390b38834c58cce16aff
satoshimarumaru <2003429satoshi@gmail.com>
2025-04-13 00:47:48 +0900

    クライアントの情報を追加

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const path = require("path")
 const mongoose = require("mongoose")
 const ejsMate = require("ejs-mate")
 app.use(express.static('public'));
+const axios = require("axios")  
+
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
 
 app.engine("ejs", ejsMate)
 app.set("view engine", "ejs");
@@ -11,6 +18,40 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({extended:true}))
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Base64エンコード（client_id:client_secret）
+const encodedCredentials = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+
+// アクセストークンを取得する関数
+async function getAccessToken() {
+try {
+    const response = await axios.post(
+    'https://accounts.spotify.com/api/token',
+    new URLSearchParams({
+        grant_type: 'client_credentials'
+    }),
+    {
+        headers: {
+        'Authorization': `Basic ${encodedCredentials}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }
+    );
+
+    const accessToken = response.data.access_token;
+    console.log('アクセストークン:', accessToken);
+    return accessToken;
+
+} catch (error) {
+    if (error.response) {
+        console.error('アクセストークンの取得に失敗:', error.response.data);
+    } else {
+        console.error('アクセストークンの取得に失敗:', error.message);
+    }
+}
+}
+
+getAccessToken()
 
 
 
@@ -27,6 +68,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/spotify',{useNewUrlParser:true,useUn
 app.get("/", (req,res) => {
     res.render("home")
 })
+
+
+console.log("クライアントID:", process.env.CLIENT_ID);
 
 
 // サーバーの立ち上げ
